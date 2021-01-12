94ebbc8196349cfe507764a8887f29596db2fda2
Samy0412 <samantha.gadet@gmail.com>
2021-01-12 00:27:19 -0500

    second commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
 
     if(this.props.data){
       var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+        return <li key={network.name}><a href={network.url} target="_blank"><i className={network.className}></i></a></li>
       })
     }
 
@@ -17,14 +17,19 @@ class Footer extends Component {
            <ul className="social-links">
               {networks}
            </ul>
-
+{/* 
            <ul>
-            <li>Modified with love by the Clever Programmer team ♥️</li>
-           </ul>
+            <li>Modified with love by Samantha Gadet ♥️</li>
+           </ul> */}
 
-           <ul className="copyright">
+           {/* <ul className="copyright">
               <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+           </ul> */}
+
+           <ul className="copyright">
+              <li>&copy; 2020 Samantha Gadet</li>
+              {/* <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li> */}
            </ul>
 
         </div>
